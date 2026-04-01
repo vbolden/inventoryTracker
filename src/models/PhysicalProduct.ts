a787df485d4ad7db73d97fb807e23254a4677858
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-01 18:13:54 -0400

    created physical product subclass

---
+++ src/models/PhysicalProduct.ts
@@ -0,0 +1,18 @@
+import { Product } from "./Product.ts"
+
+export class PhysicalProduct extends Product {
+    weight: number;
+
+    constructor(sku: string, name: string, price: number, weight: number) {
+        super(sku, name, price);
+        this.weight = weight;
+    }
+
+    override getPriceWithTax(): number {
+        return this.price * (0.10 + 1);
+    }
+
+    get formattedWeight():string {
+        return this.weight = `${this.weight} kg`;
+    }
+}
