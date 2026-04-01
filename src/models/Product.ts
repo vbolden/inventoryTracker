daa90c6dfe3850f0009d769712b8055678f405a1
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-01 18:12:23 -0400

    created product base class

---
+++ src/models/Product.ts
@@ -0,0 +1,19 @@
+export class Product {
+    private sku: string;
+    public name: string;
+    protected price: number;
+
+    constructor(sku: string, name: string, price: number) {
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
+    }
+
+    displayDetails() {
+        return `${this.name} has the sku #${this.sku} and costs $${this.price}. `
+    }
+
+    getPriceWithTax() {
+        return this.price;
+    }
+}
