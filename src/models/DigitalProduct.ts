e6083ebc96944b3825473a7ed512965e7babb26d
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-02 07:27:50 -0400

    removed .ts from the import from line

--- src/models/DigitalProduct.ts
+++ src/models/DigitalProduct.ts
@@ -1,10 +1,11 @@
-import {Product} from "./Product.ts"
+import {Product} from "./Product"
 
 export class DigitalProduct extends Product {
     fileSize: number;
 
     constructor(sku:string, name: string, price: number, fileSize: number){
         super(sku, name, price)
+        this.fileSize = fileSize;
     }
 
     // override getPriceWithTax(): number {
--- src/models/PhysicalProduct.ts
+++ src/models/PhysicalProduct.ts
@@ -1,4 +1,4 @@
-import { Product } from "./Product.ts"
+import { Product } from "./Product"
 
 export class PhysicalProduct extends Product {
     weight: number;
